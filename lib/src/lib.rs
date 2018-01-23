d07c6481c5817988e3b7b0263bcc036b360444bd
Thomas Versteeg <thomasversteeg@gmx.com>
2018-01-23 23:39:32 +0100

    Implement colorization algorithm

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,8 +1,10 @@
 #![crate_name = "sprite_gen"]
 
 extern crate rand;
+extern crate hsl;
 
 use rand::{Rng, XorShiftRng};
+use hsl::HSL;
 
 /// The options for the `gen_sprite` function.
 #[derive(Copy, Clone)]
@@ -11,7 +13,8 @@ pub struct Options {
     pub mirror_x: bool,
     /// `true` if the result buffer should be mirrored along the Y axis.
     pub mirror_y: bool,
-    /// `true` if the output should be colored. `false` if the output should be 1-bit.
+    /// `true` if the output should be colored. `false` if the output should be 1-bit. The
+    /// fields after this field only apply if `colorer` is `true`.
     pub colored: bool,
     /// A value from `0.0` - `1.0`.
     pub edge_brightness: f64,
@@ -102,45 +105,14 @@ pub fn gen_sprite(mask_buffer: &[i8], mask_width: usize, options: Options) -> Ve
         }
     }
 
-    // Convert the data to pixels
-    if !options.mirror_x && !options.mirror_y {
-        // No mirror
-        return mask.iter().map(|&v| color_output(v, &options, &rng)).collect();
-    } else if options.mirror_x && !options.mirror_y {
-        // Only mirror X
-        let width = mask_width * 2;
-        let mut result = vec![0; width * mask_height];
+    // Color the mask image
+    let colored: Vec<u32> = match options.colored {
+        true => color_output(&mask, (mask_width, mask_height), &options, &mut rng),
+        false => onebit_output(&mask)
+    };
 
-        for y in 0..mask_height {
-            for x in 0..mask_width {
-                let value = color_output(mask[x + y * mask_width], &options, &rng);
-
-                let index = x + y * width;
-                result[index] = value;
-                let index = (width - x - 1) + y * width;
-                result[index] = value;
-            }
-        }
-
-        return result;
-    } else if options.mirror_y && !options.mirror_x {
-        // Only mirror Y
-        let height = mask_height * 2;
-        let mut result = vec![0; mask_width * height];
-
-        for y in 0..mask_height {
-            for x in 0..mask_width {
-                let value = color_output(mask[x + y * mask_width], &options, &rng);
-
-                let index = x + y * mask_width;
-                result[index] = value;
-                let index = x + (height - y - 1) * mask_width;
-                result[index] = value;
-            }
-        }
-
-        return result;
-    } else {
+    // Check for mirroring
+    if options.mirror_x && options.mirror_y {
         // Mirror both X & Y
         let width = mask_width * 2;
         let height = mask_height * 2;
@@ -148,31 +120,126 @@ pub fn gen_sprite(mask_buffer: &[i8], mask_width: usize, options: Options) -> Ve
 
         for y in 0..mask_height {
             for x in 0..mask_width {
-                let value = color_output(mask[x + y * mask_width], &options, &rng);
+                let index = x + y * mask_width;
+                let value = colored[index];
 
                 let index = x + y * width;
                 result[index] = value;
+
                 let index = (width - x - 1) + y * width;
                 result[index] = value;
+
                 let index = x + (height - y - 1) * width;
                 result[index] = value;
+
                 let index = (width - x - 1) + (height - y - 1) * width;
                 result[index] = value;
             }
         }
 
         return result;
+    } else if options.mirror_x {
+        // Only mirror X
+        let width = mask_width * 2;
+        let mut result = vec![0; width * mask_height];
+
+        for y in 0..mask_height {
+            for x in 0..mask_width {
+                let index = x + y * mask_width;
+                let value = colored[index];
+
+                let index = x + y * width;
+                result[index] = value;
+
+                let index = (width - x - 1) + y * width;
+                result[index] = value;
+            }
+        }
+
+        return result;
+    } else if options.mirror_y {
+        // Only mirror Y
+        let height = mask_height * 2;
+        let mut result = vec![0; mask_width * height];
+
+        for y in 0..mask_height {
+            for x in 0..mask_width {
+                let index = x + y * mask_width;
+                let value = colored[index];
+                result[index] = value;
+
+                let index = x + (height - y - 1) * mask_width;
+                result[index] = value;
+            }
+        }
+
+        return result;
     }
+    
+    return colored;
 }
 
 #[inline]
-fn color_output(input: i8, options: &Options, rng: &XorShiftRng) -> u32 {
-    if !options.colored {
-        return match input {
-            -1 => 0,
-            _ => 0xFFFFFFFF
-        };
+fn onebit_output(mask: &[i8]) -> Vec<u32> {
+    mask.iter().map(|&v| match v {
+        0 => 0xFFFFFFFF,
+        _ => 0
+    }).collect()
+}
+
+#[inline]
+fn color_output(mask: &[i8], mask_size: (usize, usize), options: &Options, rng: &mut XorShiftRng) -> Vec<u32> {
+    let mut result = vec![0xFFFFFFFF; mask.len()];
+    
+    let is_vertical_gradient = rng.next_f32() > 0.5;
+    let saturation = (rng.next_f64() * options.saturation).max(0.0).min(1.0);
+    let mut hue = rng.next_f64();
+
+    let variation_check = 1.0 - options.color_variations;
+    let brightness_inv = 1.0 - options.brightness_noise;
+
+    let uv_size = match is_vertical_gradient {
+        true => (mask_size.1, mask_size.0),
+        false => mask_size
+    };
+
+    for u in 0..uv_size.0 {
+        let is_new_color = ((rng.gen_range(-1.0, 1.0) + rng.gen_range(-1.0, 1.0) + rng.gen_range(-1.0, 1.0)) / 3.0 as f64).abs();
+
+        if is_new_color > variation_check {
+            hue = rng.next_f64();
+        }
+
+        for v in 0..uv_size.1 {
+            let index = match is_vertical_gradient {
+                true => v + u * mask_size.0,
+                false => u + v * mask_size.0
+            };
+
+            let val = mask[index];
+            if val == 0 {
+                continue;
+            }
+
+            let u_sin = ((u as f64 / uv_size.0 as f64) * std::f64::consts::PI).sin();
+            let brightness = u_sin * brightness_inv + rng.gen_range(0.0, options.brightness_noise);
+
+            let mut rgb = HSL {
+                h: hue,
+                s: saturation,
+                l: brightness
+            }.to_rgb();
+
+            // Make the edges darker
+            if val == -1 {
+                rgb.0 = (rgb.0 as f64 * options.edge_brightness) as u8;
+                rgb.1 = (rgb.1 as f64 * options.edge_brightness) as u8;
+                rgb.2 = (rgb.2 as f64 * options.edge_brightness) as u8;
+            }
+
+            result[index] = ((rgb.0 as u32) << 16) | ((rgb.1 as u32) << 8) | (rgb.2 as u32);
+        }
     }
 
-    0
+    result
 }
